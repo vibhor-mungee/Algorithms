2cfa6a5178326f18a96b4e9154ac5d787673b34c
vibhor.mungee <vibhor.mungee@gmail.com>
2013-08-28 13:03:04 +0530

    v2.0 Bubble sort implemented

--- Algorithms/src/com/example/algorithms/BubbleSort.java
+++ Algorithms/src/com/example/algorithms/BubbleSort.java
@@ -1,6 +1,7 @@
 package com.example.algorithms;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Iterator;
 
 import android.app.Activity;
@@ -11,25 +12,33 @@ import android.widget.TextView;
 
 public class BubbleSort extends Activity {
 
+	public StringBuffer sb = new StringBuffer();
+
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		
-		StringBuffer sb = new StringBuffer();
+
 		Intent in = getIntent();
 		Bundle b = in.getExtras();
-		if(b!=null){
+
+		if (b != null) {
 			TextView tv1 = new TextView(this);
-			ArrayList<String> arr = (ArrayList<String>)b.getStringArrayList("Input");
+			ArrayList<String> arr = (ArrayList<String>) b
+					.getStringArrayList("Input");
+			int[] inputInt = new int[arr.size()];
+			int n = 0;
 			Iterator it = arr.iterator();
-			while(it.hasNext()){
-				sb.append(it.next() + " ");
+			while (it.hasNext()) {
+				inputInt[n] = Integer.parseInt((String) it.next());
+				n++;
 			}
+			sb.append("Input Data" + Arrays.toString(inputInt));
+			int[] output = BubSort(inputInt);
+			sb.append("\nOutput Sorted Data: " + Arrays.toString(output));
 			tv1.setText(sb);
 			setContentView(tv1);
-			
 		}
-		
+
 	}
 
 	@Override
@@ -37,6 +46,39 @@ public class BubbleSort extends Activity {
 		// Inflate the menu; this adds items to the action bar if it is present.
 		getMenuInflater().inflate(R.menu.bubble_sort, menu);
 		return true;
+	}
+	
+	public int[] BubSort(int[] inp){
+
+		
+		int[] input = inp;
+		int i = 0;
+		int flag = 1;
+		sb.append("\nSorting Start");
+		int pass = 0;
+		while(flag==1){
+			sb.append("\nPass: " + pass + ".");
+			sb.append("\n" + Arrays.toString(input));
+			pass++;
+			flag = 0;
+			i=0;
+			while (i<(input.length-1)){
+				int temp = 0;
+				if(input[i]>input[i+1]){
+					temp = input[i];
+					input[i] = input[i+1];
+					input[i+1] = temp;
+					flag = 1;
+				}
+				i++;
+			}	
+		}
+		sb.append("\nSorting End.");
+		return input;
+		
+	
+		
+		
 	}
 
 }
